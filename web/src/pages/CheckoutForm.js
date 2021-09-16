9363309e2636786cbbd0961567d3852b3edfa881
lepusarcticus <ashutoshpande29@gmail.com>
2021-09-16 15:17:56 +0530

    Stripe Checkout Form MUI

--- web/src/pages/CheckoutForm.js
+++ web/src/pages/CheckoutForm.js
@@ -1,8 +1,13 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
+import { CardNumberElement, CardExpiryElement, CardCvcElement } from "@stripe/react-stripe-js";
+import {ArrowForward } from '@material-ui/icons';
 import axios from "axios"
 import React, { useState } from 'react'
 import './styles.css';
-
+import { Alert } from "@material-ui/core";
+import { LoadingButton } from "@material-ui/lab";
+import {Button, TextField, Paper, Grid } from "@material-ui/core";
+import StripeInput from "../../src/components/StripeInput";
 const CARD_OPTIONS = {
 	iconStyle: "solid",
 	style: {
@@ -27,14 +32,19 @@ export default function PaymentForm() {
     const [success, setSuccess ] = useState(false)
     const stripe = useStripe()
     const elements = useElements()
-
+    const [loading, setLoading] = React.useState(false);
 
     const handleSubmit = async (e) => {
+        setLoading(true);
         e.preventDefault()
         const {error, paymentMethod} = await stripe.createPaymentMethod({
             type: "card",
-            card: elements.getElement(CardElement)
+            card: elements.getElement(CardNumberElement)
         })
+
+        
+
+
 
 
     if(!error) {
@@ -58,23 +68,102 @@ export default function PaymentForm() {
     }
 }
 
+const paperStyle={padding:'30px 20px', width: 600, margin: "20px auto"}
+const marginTop = { marginTop: 10} 
     return (
+        <center>
         <>
         {!success ? 
-        <form onSubmit={handleSubmit}>
-            <fieldset className="FormGroup">
-                <div className="FormRow">
-                    <CardElement options={CARD_OPTIONS}/>
-                </div>
-            </fieldset>
-            <button>Pay</button>
+          <Grid>
+          <Paper  elevation ={20} style = {paperStyle} >
+          <Grid align='center'></Grid>
+
+
+        <form>
+        <Alert severity="info">Contribute $10 for Open Source</Alert>
+        <Grid  item xs ={12}></Grid>
+           <Grid item xs={12} sm={6}>
+            <TextField 
+            //placeholder="XXXX XXXX XXXX XXXX"
+            label="Card Number"
+            style={marginTop}
+            fullWidth
+            InputLabelProps={{
+                shrink: true,
+              }}
+            InputProps={{
+                inputComponent: StripeInput,
+                inputProps: {
+                     component: CardNumberElement
+                },
+           }}
+            />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+            <TextField 
+            //placeholder="MM/YY"
+            fullWidth
+            label="Expiry Date"
+            style={marginTop}
+            InputLabelProps={{
+                shrink: true,
+              }}
+            InputProps={{
+                inputComponent: StripeInput,
+                inputProps: {
+                    component: CardExpiryElement
+                },
+            }}
+            />
+            </Grid>
+            <Grid  item xs ={12}></Grid>
+            <Grid item xs={12} sm={6}>
+            <TextField 
+            //placeholder="XXX"
+            fullWidth
+            label="CVV"
+            style={marginTop}
+            InputLabelProps={{
+                shrink: true,
+              }}
+            InputProps={{
+                inputComponent: StripeInput,
+                inputProps: {
+                    component: CardCvcElement
+                },
+            }}
+            />
+            </Grid>
+            
+            <Grid  item xs ={12}></Grid>
+            <Grid item xs={12} sm={6}>
+            <LoadingButton 
+            loading ={loading}
+            loadingPosition="start"
+            startIcon={<ArrowForward />}
+            fullWidth 
+            variant="outlined"  
+            onClick={handleSubmit}
+            style={marginTop}>
+                Pay
+            </LoadingButton>
+            </Grid>
         </form>
+        </Paper>
+       </Grid>
         :
-       <div>
-           <h2>Payment Successful!!!</h2>
-       </div> 
+        <Grid>
+          <Paper  elevation ={20} style = {paperStyle} >
+          <Grid align='center'></Grid>
+       <Alert severity="success">
+           Your payment is Successful. Thanks for using our service.
+       </Alert> 
+       </Paper>
+       </Grid>
+      
         }
             
         </>
+        </center>
     )
 }
