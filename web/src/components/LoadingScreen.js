b85da20c8dc6ca40e6c34589e4ae0409a2785e3c
zcon-anant-raut <anant.raut@zconsolutions.com>
2021-11-16 15:34:53 +0530

    Added circular progress widget for loading screen

--- web/src/components/LoadingScreen.js
+++ web/src/components/LoadingScreen.js
@@ -6,6 +6,7 @@ import { alpha, experimentalStyled as styled } from '@mui/material/styles';
 import makeStyles from '@mui/styles/makeStyles';
 import { Box } from '@mui/material';
 //
+import CircularProgress from '@mui/material/CircularProgress';
 import Logo from './Logo';
 
 // ----------------------------------------------------------------------
@@ -59,60 +60,70 @@ export default function LoadingScreen({ ...other }) {
     NProgress.done();
   }, []);
 
-  return (
-    <RootStyle {...other}>
-      <motion.div
-        initial={{ rotateY: 0 }}
-        animate={{ rotateY: 360 }}
-        transition={{
-          duration: 2,
-          ease: 'easeInOut',
-          repeatDelay: 1,
-          repeat: Infinity
-        }}
-      >
-        <Logo sx={{ width: 64, height: 64 }} />
-      </motion.div>
+  const hideLoader = true;
+  if (!hideLoader) {
+    return (
+      <RootStyle {...other}>
+        <motion.div
+          initial={{ rotateY: 0 }}
+          animate={{ rotateY: 360 }}
+          transition={{
+            duration: 2,
+            ease: 'easeInOut',
+            repeatDelay: 1,
+            repeat: Infinity
+          }}
+        >
+          <Logo sx={{ width: 64, height: 64 }} />
+        </motion.div>
 
-      <Box
-        component={motion.div}
-        animate={{
-          scale: [1.2, 1, 1, 1.2, 1.2],
-          rotate: [270, 0, 0, 270, 270],
-          opacity: [0.25, 1, 1, 1, 0.25],
-          borderRadius: ['25%', '25%', '50%', '50%', '25%']
-        }}
-        transition={{ ease: 'linear', duration: 3.2, repeat: Infinity }}
-        sx={{
-          width: 100,
-          height: 100,
-          borderRadius: '25%',
-          position: 'absolute',
-          border: (theme) => `solid 3px ${alpha(theme.palette.primary.dark, 0.24)}`
-        }}
-      />
+        <Box
+          component={motion.div}
+          animate={{
+            scale: [1.2, 1, 1, 1.2, 1.2],
+            rotate: [270, 0, 0, 270, 270],
+            opacity: [0.25, 1, 1, 1, 0.25],
+            borderRadius: ['25%', '25%', '50%', '50%', '25%']
+          }}
+          transition={{ ease: 'linear', duration: 3.2, repeat: Infinity }}
+          sx={{
+            width: 100,
+            height: 100,
+            borderRadius: '25%',
+            position: 'absolute',
+            border: (theme) => `solid 3px ${alpha(theme.palette.primary.dark, 0.24)}`
+          }}
+        />
 
-      <Box
-        component={motion.div}
-        animate={{
-          scale: [1, 1.2, 1.2, 1, 1],
-          rotate: [0, 270, 270, 0, 0],
-          opacity: [1, 0.25, 0.25, 0.25, 1],
-          borderRadius: ['25%', '25%', '50%', '50%', '25%']
-        }}
-        transition={{
-          ease: 'linear',
-          duration: 3.2,
-          repeat: Infinity
-        }}
-        sx={{
-          width: 120,
-          height: 120,
-          borderRadius: '25%',
-          position: 'absolute',
-          border: (theme) => `solid 8px ${alpha(theme.palette.primary.dark, 0.24)}`
-        }}
-      />
-    </RootStyle>
-  );
+        <Box
+          component={motion.div}
+          animate={{
+            scale: [1, 1.2, 1.2, 1, 1],
+            rotate: [0, 270, 270, 0, 0],
+            opacity: [1, 0.25, 0.25, 0.25, 1],
+            borderRadius: ['25%', '25%', '50%', '50%', '25%']
+          }}
+          transition={{
+            ease: 'linear',
+            duration: 3.2,
+            repeat: Infinity
+          }}
+          sx={{
+            width: 120,
+            height: 120,
+            borderRadius: '25%',
+            position: 'absolute',
+            border: (theme) => `solid 8px ${alpha(theme.palette.primary.dark, 0.24)}`
+          }}
+        />
+      </RootStyle>
+    );
+  }
+  if (hideLoader) {
+    return (
+      <RootStyle {...other}>
+        <CircularProgress color="success" disableShrink size={90} thickness={4} />
+      </RootStyle>
+    );
+  }
 }
