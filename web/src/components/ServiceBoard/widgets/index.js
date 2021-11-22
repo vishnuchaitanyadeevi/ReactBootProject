195193a8fd6c72a80f48c2d32677901792fd005c
Sanket Sonawane <sanket.sonawane@zconsolutions.com>
2021-11-23 01:48:18 +0530

    code refactored

--- web/src/components/ServiceBoard/widgets/index.js
+++ web/src/components/ServiceBoard/widgets/index.js
@@ -1,9 +1,5 @@
 import DeleteButton from './DeleteButton'
-import EditableLabel from './EditableLabel'
-import InlineInput from './InlineInput'
 
 export default {
-  DeleteButton,
-  EditableLabel,
-  InlineInput
+  DeleteButton
 }
